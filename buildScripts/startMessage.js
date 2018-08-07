3e5e8960e611bc125734d406b47f77874f0d3fae
Jesus Nunez <jesus.nunez@incontact.com>
2018-08-07 16:31:21 -0400

    configuring babel for transpiling to ES6

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
-var chalk = require ('chalk');
+//var chalk = require ('chalk');
+import chalk from 'chalk'; // standart implement in es6
+
 
 console.log(chalk.green('Starting app in dev mode...'));
